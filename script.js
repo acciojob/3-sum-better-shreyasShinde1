9d66f1603105d7babfd0a6c19bba2440cad8aba9
Shreyas Shinde <130788791+shreyasShinde1@users.noreply.github.com>
2023-08-17 13:11:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,32 @@
 function threeSum(arr, target) {
 // write your code here
+	arr.sort((a, b) => a - b);
+
+  let closestSum = arr[0] + arr[1] + arr[2];
+
+  for (let i = 0; i < arr.length - 2; i++) {
+    let left = i + 1;
+    let right = arr.length - 1;
+
+    while (left < right) {
+      const sum = arr[i] + arr[left] + arr[right];
+
+      if (Math.abs(sum - target) < Math.abs(closestSum - target)) {
+        closestSum = sum;
+      }
+
+      if (sum < target) {
+        left++;
+      } else if (sum > target) {
+        right--;
+      } else {
+        // Found an exact match, so return the sum
+        return sum;
+      }
+    }
+  }
+
+  return closestSum;
   
 }
 
